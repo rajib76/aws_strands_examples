ecfa7ccec5d8eb40fe44d1dca6829ceba8694285
rajib76 <rajib76.gcp@gmail.com>
2025-05-17 20:48:02 -0700

    change region for 01_hello_strands

--- examples/01_hello_strands.py
+++ examples/01_hello_strands.py
@@ -10,25 +10,28 @@ load_dotenv()
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+boto_session = boto3.session.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
+                                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                                     region_name="us-east-1")
 
-# Create a BedrockModel
-# model = BedrockModel(
-#     model_id="us.anthropic.claude-3-7-sonnet-20250219-v1:0",
-#     temperature=0.3
-# )
-
-
-model = LiteLLMModel(
-    client_args={
-        "api_key": OPENAI_API_KEY,
-    },
-    # **model_config
-    model_id="openai/gpt-4o",
-    params={
-        "max_tokens": 1000,
-        "temperature": 0.7,
-    }
+model = BedrockModel(
+    model_id="us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+    temperature=0.3,
+    boto_session=boto_session
 )
+
+
+# model = LiteLLMModel(
+#     client_args={
+#         "api_key": OPENAI_API_KEY,
+#     },
+#     # **model_config
+#     model_id="openai/gpt-4o",
+#     params={
+#         "max_tokens": 1000,
+#         "temperature": 0.7,
+#     }
+# )
 
 agent = Agent(model=model)
 
